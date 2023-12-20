8363a55f5cacbd3f8b42a414b9a927d6ccdad3fa
Onur Haktan <onurhaktan243@gmail.com>
2023-12-21 00:23:52 +0300

    create RegionsController and add GetAll endpoint

---
+++ NZWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        // Get All Regions
+        // GET: https://localhost:portnumber/api/regions
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var regions = new List<Region>
+            {
+                new Region
+                {
+                    id = Guid.NewGuid(),
+                    Name = "Auckland Region",
+                    Code = "AKL",
+                    RegionImageUrl = "https://www.pexels.com/tr-tr/fotograf/agaclar-park-yuksek-kule-18742687/"
+                },
+                new Region
+                {
+                    id = Guid.NewGuid(),
+                    Name = "Wellington Region",
+                    Code = "WLG",
+                    RegionImageUrl = "https://www.pexels.com/tr-tr/fotograf/agaclar-park-yuksek-kule-18742687/"
+                }
+            };
+            return Ok(regions);
+        }
+    }
+}
